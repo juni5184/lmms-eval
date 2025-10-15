f4caf1e22fd0a829ea2bf97e22cc12bff8beead0
juni5184 <juni5184@yonsei.ac.kr>
2025-10-15 11:39:32 +0000

    update model 7b to 0.5b

--- examples/models/llava_onevision.sh
+++ examples/models/llava_onevision.sh
@@ -5,6 +5,6 @@ export HF_HOME="~/.cache/huggingface"
 
 accelerate launch --num_processes=8 --main_process_port 12399 -m lmms_eval \
     --model=llava_onevision \
-    --model_args=pretrained=lmms-lab/llava-onevision-qwen2-7b-ov,conv_template=qwen_1_5,device_map=auto,model_name=llava_qwen \
+    --model_args=pretrained=lmms-lab/llava-onevision-qwen2-0.5b-ov,conv_template=qwen_1_5,device_map=auto,model_name=llava_qwen \
     --tasks=ai2d,chartqa,docvqa_val,mmmu_pro \
     --batch_size=1
